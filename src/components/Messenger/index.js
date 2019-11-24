ea330c136df63c9ebd50cae1508b1b6305d5f6dd
Yegor Druzhinin <e.drujinin@skbkontur.ru>
2019-11-24 15:14:34 +0500

    Добавил правый sidebar

--- src/components/Messenger/index.js
+++ src/components/Messenger/index.js
@@ -8,12 +8,16 @@ import './Messenger.css'
 export default function Messenger(props) {
     return (
         <div className="messenger">
-            <div className="scrollable sidebar">
+            <div className="scrollable left-sidebar">
                 <ConversationList />
             </div>
 
             <div className="scrollable content">
                 <MessageList />
+            </div>
+
+            <div className="right-sidebar">
+
             </div>
         </div>
     );
