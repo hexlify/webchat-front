4231b28af10ed4a0b5c7a43134e066f3273f9649
Yegor Druzhinin <e.drujinin@skbkontur.ru>
2019-12-26 12:22:34 +0500

    Updates

---
+++ precache-manifest.e2fbcf4166836dd2285d47cafef1f753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761dc78a0a143bbf6cb510ce10aa9360",
+    "url": "/webchat-front/index.html"
+  },
+  {
+    "revision": "be60d842b69b5f5bc071",
+    "url": "/webchat-front/static/css/main.3c796dd9.chunk.css"
+  },
+  {
+    "revision": "53b96923e4e3237b56d1",
+    "url": "/webchat-front/static/js/2.8f4c9ec7.chunk.js"
+  },
+  {
+    "revision": "35df66abd0e50ae55e0d135085cf6d8c",
+    "url": "/webchat-front/static/js/2.8f4c9ec7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be60d842b69b5f5bc071",
+    "url": "/webchat-front/static/js/main.ead7dbdf.chunk.js"
+  },
+  {
+    "revision": "c50ef7b5574e0895238b",
+    "url": "/webchat-front/static/js/runtime-main.2a668412.js"
+  }
+]);
